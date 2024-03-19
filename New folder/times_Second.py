a5ffdd0a6bbd2043fbba1e88a3b23312d712b7de
Sepehr Jamali <www.teryak123@gmail.com>
2024-03-19 20:09:14 +0330

    Tamrin

--- New folder/times_Second.py
+++
@@ -1,7 +0,0 @@
-times = 0
-hours = int(input("Enter the hours:  "))
-minutes = int(input("Enter the minutes: "))
-Second =int(input("Enter the seconds: "))
-times = (hours* 3600) + (minutes*60) + Second
-
-print("Secouns: ", times)
